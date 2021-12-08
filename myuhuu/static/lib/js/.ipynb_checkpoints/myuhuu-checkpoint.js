28ca144e015e169bd4c852a556cc26038445c833
Obed Gonzalez <cesarg@merxbp.com>
2021-12-08 23:10:48 +0000

    leyendo session

--- myuhuu/static/lib/js/.ipynb_checkpoints/myuhuu-checkpoint.js
+++ myuhuu/static/lib/js/.ipynb_checkpoints/myuhuu-checkpoint.js
@@ -1,5 +1,7 @@
 if( !window.UHUUWidget ){
         window.uhuuOdoo = {};
+        var session = require('web.session');
+        console.log(session)
         var h = document.getElementsByTagName('script')[0], j = document.createElement('script');
         j.async = false; 
         j.src = 'https://1a62-177-232-83-239.ngrok.io/uhuuWidgets.js';//Revisar url correcta
