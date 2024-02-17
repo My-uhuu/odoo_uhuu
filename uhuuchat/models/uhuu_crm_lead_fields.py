fbd07c3a84fb157f40513c3cf616b11577656657
COGN AKA Kobedinho <coben.9@gmail.com>
2024-02-16 18:00:09 -0600

    Ajustes para v17

---
+++ uhuuchat/models/uhuu_crm_lead_fields.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+class uhuu_crm_lead_fields(models.Model):
+
+    _inherit = 'crm.lead'
+
+    x_facebookId = fields.Text(string='Facebook ID (Uhuu!)', default=False)
+    x_telegramId= fields.Text(string='Telegram ID (Uhuu!)', default=False)
+    x_twitterId = fields.Text(string='Twitter ID (Uhuu!)', default=False)
